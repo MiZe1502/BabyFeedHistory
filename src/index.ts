c79e43966fa9653d7888c4ed4c1fc840cd0cf46c
MiZe1502 <z.com@mail.ru>
2021-02-19 00:09:12 +0300

    init apollo-server-express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("it works")
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = 3000;
+const host = 'http://localhost'
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+    },
+};
+
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+const app = express();
+
+server.applyMiddleware({ app });
+
+app.listen({ port }, () =>
+    console.log(`Server is running at ${host}:${port}${server.graphqlPath}`)
+);
