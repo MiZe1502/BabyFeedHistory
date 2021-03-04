cba595185b00559aed3d0f8b7868f98ea1548346
MiZe1502 <z.com@mail.ru>
2021-03-04 21:29:01 +0300

    implemented repository for feed details

--- src/db/repos/feedDetails.ts
+++ src/db/repos/feedDetails.ts
@@ -1,5 +1,6 @@
 import {FeedDetailsData} from "../schemas/feedDetails";
 import {feedDetailsModel} from "../models/feedDetails";
+import {QueryResult} from "../../utils/types";
 
 export const getAvailableFeedDetails = async (userLogin: string):
     Promise<FeedDetailsData[] | null> => feedDetailsModel.find({createdBy: userLogin})
@@ -8,3 +9,24 @@ export const createNewFeedDetails =
     async (userLogin: string, feedDetailsItem: FeedDetailsData):
         Promise<FeedDetailsData | null> =>
         feedDetailsModel.create({...feedDetailsItem, createdBy: userLogin})
+
+export const updateFeedDetailsItem =
+    async (userLogin: string, oldName: string, newFeedDetailsData: FeedDetailsData):
+        QueryResult => {
+    return feedDetailsModel.updateOne({
+      $and: [
+          { name: oldName },
+          { createdBy: userLogin }
+      ]
+    }, newFeedDetailsData)
+}
+
+export const removeFeedDetailsItem =
+    async (userLogin: string, name: string): QueryResult => {
+    return feedDetailsModel.remove({
+        $and: [
+            { name },
+            { createdBy: userLogin }
+        ]
+    })
+}
