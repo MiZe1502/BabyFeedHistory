e16ee07da7f0d1f0aea88a21fe756a72a1fbc293
MiZe1502 <z.com@mail.ru>
2021-03-16 23:59:18 +0300

    started gql on client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import {ApolloProvider, gql, useMutation} from "@apollo/client";
+import {client} from "./api";
 
-class App extends React.Component {
-    render() {
-        return(
-            <div>
+const MUTATION_AUTH = gql`
+    mutation Auth($login: String!, $password: String!) {
+        login(login: $login, password: $password)
+    }
+`
+
+export const App = () => {
+    const [auth, data] = useMutation(MUTATION_AUTH);
+
+    auth({ variables: { login: "abs updated login 2", password: "qwerty12" } })
+        .then((res) => console.log(res));
+
+    console.log(data)
+    return <ApolloProvider client={client}>
+        <div>
             My App Component
         </div>
-    );
-    }
+    </ApolloProvider>
 }
 
 export default App
