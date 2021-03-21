37b601500f8de9ddd4cfe294ff48ecd356fe3755
MiZe1502 <z.com@mail.ru>
2021-03-21 14:02:04 +0300

    started login page

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -5,9 +5,11 @@ import {
     DialogContentText,
     DialogTitle, TextField
 } from "@material-ui/core";
-import React from "react";
+import React, {useState} from "react";
 import {gql, useMutation} from "@apollo/client";
+import {useAuth} from "../../common/hooks/useAuth";
 
+import css from "./LoginPage.scss"
 
 const MUTATION_AUTH = gql`
     mutation Auth($login: String!, $password: String!) {
@@ -15,37 +17,71 @@ const MUTATION_AUTH = gql`
     }
 `
 
+export interface LoginResp {
+    login: string;
+}
+
 export const LoginPage = () => {
-    const [auth, data] = useMutation(MUTATION_AUTH);
+    const auth = useAuth();
+
+    const [authMethod, data] = useMutation<LoginResp>(MUTATION_AUTH);
+
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
 
     const signIn = (login: string, password: string) => {
-        auth({ variables: { login: "abs updated login 2", password: "qwerty12" } })
-            .then((res) => console.log(res));
+        authMethod({ variables: { login, password } })
+            .then((res) => {
+                console.log(auth, res, data)
+                auth?.updateToken(res?.data?.login || "")
+            });
+    }
+
+    const onLoginChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLogin(event.target.value)
+    }
+
+    const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
+    const onSignInClick = () => {
+        // signIn("abs updated login 2", "qwerty12")
+        signIn(login, password)
     }
 
     return <div>
         <Dialog open={true} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+            <DialogTitle id="form-dialog-title">Authorize</DialogTitle>
             <DialogContent>
-                <DialogContentText>
-                    To subscribe to this website, please enter your email address here. We will send updates
-                    occasionally.
-                </DialogContentText>
                 <TextField
                     autoFocus
                     margin="dense"
-                    id="name"
+                    id="login"
                     label="Email Address"
                     type="email"
                     fullWidth
+                    value={login}
+                    onChange={onLoginChange}
+                    className={css.LoginField}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="password"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    value={password}
+                    onChange={onPasswordChange}
                 />
             </DialogContent>
             <DialogActions>
-                <Button onClick={() => {}} color="primary">
-                    Cancel
+                <Button onClick={onSignInClick} color="primary">
+                    Sign In
                 </Button>
-                <Button onClick={() => {}} color="primary">
-                    Subscribe
+                <Button onClick={() => {}} color="secondary">
+                    Sign Up
                 </Button>
             </DialogActions>
         </Dialog>
