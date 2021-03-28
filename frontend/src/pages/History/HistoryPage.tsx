dc98b5bc397880a3c47741adb75625931f65db69
MiZe1502 <z.com@mail.ru>
2021-03-28 23:54:46 +0300

    started calendar component

--- frontend/src/pages/History/HistoryPage.tsx
+++ frontend/src/pages/History/HistoryPage.tsx
@@ -1,5 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
+import dateFns from "date-fns";
+// //@ts-ignore
+// import Calendar from "react-material-ui-calendar";
+
+import css from "./HistoryPage.scss";
 
 export const HistoryPage = (): React.ReactElement => {
-    return <div>HISTORY PAGE</div>
+    const [currentMonth, setCurrentMonth] = useState(new Date())
+    const [selectedDate, setSelectedDate] = useState(new Date())
+
+    const renderHeader = () => {
+        const dateFormat = "MMMM YYYY";
+    }
+
+    const renderDaysTitle = () => {
+        const dateFormat = "dddd";
+        const days = [];
+
+        const startDate = dateFns.startOfWeek(currentMonth);
+
+        for (let i = 0; i < 7; i++) {
+            days.push(
+                <div className={css.Item} key={i}>
+                    {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+                </div>
+            );
+        }
+
+        return <div className={css.Row}>{days}</div>;
+    }
+
+    const renderCells = () => {
+        const monthStart = dateFns.startOfMonth(currentMonth);
+        const monthEnd = dateFns.endOfMonth(monthStart);
+        const startDate = dateFns.startOfWeek(monthStart);
+        const endDate = dateFns.endOfWeek(monthEnd);
+
+        const dateFormat = "D";
+        const rows = [];
+
+        let days = [];
+        let day = startDate;
+        let formattedDate = "";
+
+        while (day <= endDate) {
+            for (let i = 0; i < 7; i++) {
+                formattedDate = dateFns.format(day, dateFormat);
+
+                days.push(<div className={css.Item}>
+                    <span>{formattedDate}</span>
+                </div>)
+
+                day = dateFns.addDays(day, 1);
+            }
+
+            rows.push(<div className={css.Row}>
+                {days}
+            </div>)
+
+            days = [];
+        }
+
+        return rows
+    }
+
+
+    return (
+        <div className="calendar">
+            {renderDaysTitle()}
+            {renderCells()}
+        </div>
+    );
 }
