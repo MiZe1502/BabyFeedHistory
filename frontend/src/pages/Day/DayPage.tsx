e2b9372602ad828117208e257c3deec095d5bbd6
MiZe1502 <z.com@mail.ru>
2021-04-08 23:56:44 +0300

    started working with cached data

--- frontend/src/pages/Day/DayPage.tsx
+++ frontend/src/pages/Day/DayPage.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import {client} from "../../api";
+import {FeedsResp, FeedsVariables, QUERY_GET_FEEDS} from "../History/api";
+import dateFns from "date-fns";
 
 export const DayPage = () => {
+    const [currentDate, setCurrentDate] = useState(new Date())
+    const cachedData = client.readQuery<FeedsResp>({
+        query: QUERY_GET_FEEDS,
+        variables: { year: dateFns.getYear(currentDate),
+            month: dateFns.getMonth(currentDate)
+        }})?.lastMonthFeeds;
+
+    console.log(cachedData)
+
     return <div>DAY PAGE</div>
 }
